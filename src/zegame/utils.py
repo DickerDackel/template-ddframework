ba6b30988e7b2237ed4bc40429a9546f26b5b134
Michael Lamertz <michael.lamertz@gmail.com>
2026-05-26 19:15:22 +0200

    prettyfied plus random_vector function

--- src/zegame/utils.py
+++ src/zegame/utils.py
@@ -1,5 +1,8 @@
+from random import random
+
 import json
 
+import glm
 import pygame
 import pygame._sdl2.video as sdl2
 
@@ -9,10 +12,16 @@ def load_spritesheet(fname, renderer):
     texture = sdl2.Texture.from_surface(renderer, image)
 
     with open(fname.with_suffix('.json')) as f:
-        rects = json.load(f)
+        cells = json.load(f)
 
-    res = {}
-    for k, v in rects.items():
-        res[k] = pygame.Rect(v)
+    sprites = {name: pygame.Rect(rect) for name, rect in cells.items()}
 
-    return texture, res
+    return texture, sprites
+
+
+def random_vector(min_speed, max_speed):
+    angle = glm.radians(random() * 360)
+    length = min_speed + random() * (max_speed - min_speed)
+    v = glm.vec2(1, 0) * length
+
+    return glm.rotate(v, angle)
